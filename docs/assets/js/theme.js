ce96a6408576787b49263be183c10aacffba15ed
Kitsune Solar <kitsune.solar@gmail.com>
2019-02-17 13:43:26 +0300

    2019-02-17 10:43:26

--- docs/assets/js/theme.js
+++ docs/assets/js/theme.js
@@ -1,9 +1,10 @@
 'use strict';
 
 discordMessages.run(
-	'#data-updates',
 	'303327594793795588',
 	'442426422594633738',
 	'10',
-	'TlRRd01URTJOakV6TWpNeU16STROekEwLkR6TU9jUS4weFRCQVpiekFVcUtxRjNiRVJ4OHJlaDRRUWc='
+	'TlRRd01URTJOakV6TWpNeU16STROekEwLkR6TU9jUS4weFRCQVpiekFVcUtxRjNiRVJ4OHJlaDRRUWc=',
+	'feed',
+	'#data-updates'
 );
